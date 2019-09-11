e7feb6ffadee1b8efb05e3c20ec478fb4f41021c
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-11 21:24:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,5 +7,14 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = {}
         end
         
-        if new_hash[name]
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        
+        new_hash[name][key] << value2.to_s
+        
+      end
+    end
+  end
+  new_hash
 end
