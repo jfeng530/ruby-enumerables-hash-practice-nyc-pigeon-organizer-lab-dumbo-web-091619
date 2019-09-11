254407c1db35204f940c3d5c5bf9248d66ff5bb0
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-11 21:19:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,5 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |item|
-    item.each do |subitem|
-      if new_hash
+  data.each do |key, value|
+    key.each do |key2, value2|
 end
